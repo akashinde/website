844b85f6cfca29376ffafb53f229e1940d7cae77
akashinde <akashvshinde2001@gmail.com>
2025-06-04 14:26:13 +0200

    fix path configuration

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
-    assetPrefix: isProd ? '/your-repository-name/' : '',
+    assetPrefix: isProd ? '/website/' : '',
     basePath: isProd ? '/website' : '',
     output: 'export',
 };
